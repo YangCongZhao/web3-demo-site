b86f7ba9310bb632fc7ef1b1c7624ad413f75a95
DESKTOP-9N45CRK\Yangc <15315575962@163.com>
2025-05-10 17:14:18 +0800

    init

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,15 +1,8 @@
 export const runtime = 'edge';
 import { NextResponse } from "next/server";
-import { serialize } from "cookie";
 export async function POST() {
-    const cookie = serialize("auth", "true", { path: "/", httpOnly: true });
     return NextResponse.json(
       { message: "Access granted" },
-      {
-        headers: {
-          "Set-Cookie": cookie,
-        },
-      }
     );
 }
 //hello
